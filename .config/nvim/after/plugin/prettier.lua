c1c3555d0c4df8cbb696a67c7e68922003b2d8ef
Aaron Bos <aaronmbos@gmail.com>
2023-06-04 06:18:50 -0500

    Remove Neovim plugins (for now)

--- .config/nvim/after/plugin/prettier.lua
+++
@@ -1,3 +0,0 @@
-vim.cmd([[let g:prettier#autoformat = 1]]);
-vim.cmd([[let g:prettier#autoformat_require_pragma = 0]]);
-vim.cmd([[let g:prettier#config#tab_width = 2]]);
--- .config/nvim/lua/aaronb/packer.lua
+++ .config/nvim/lua/aaronb/packer.lua
@@ -7,8 +7,6 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use 'folke/tokyonight.nvim'
-
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
@@ -16,46 +14,8 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-lualine/lualine.nvim'
-    -- Eventually when I have Monocode patched
+    -- Requires a NerdFont
     -- requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-
-  use {
-    'kyazdani42/nvim-tree.lua'
-    -- requires = {
-    --   'kyazdani42/nvim-web-devicons', -- optional, for file icons
-    -- },
-    -- tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  }
-
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
-
-  use {"akinsho/toggleterm.nvim", tag = '*'}
-
-  use {
-    "folke/trouble.nvim"
-    -- requires = "kyazdani42/nvim-web-devicons",
-  }
-
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-
-  use 'mfussenegger/nvim-dap'
-  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-
-  use {
-    'prettier/vim-prettier',
-    run = 'npm install --frozen-lockfile --production'
   }
 
 end)
