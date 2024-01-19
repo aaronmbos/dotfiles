56c154d401900004fed871cbcf7b89e8019e47f0
Aaron Bos <aaronmbos@gmail.com>
2024-01-19 07:55:35 -0600

    Vanilla neovim

--- .config/nvim/after/plugin/color.lua
+++
@@ -1,8 +0,0 @@
-hour = os.date("*t")["hour"]
-
-if hour >= 16 or hour < 8 then 
-  vim.opt.background = "dark"
-end
-
-vim.opt.termguicolors = true
-
--- .config/nvim/lua/aaronb/init.lua
+++
@@ -1,2 +0,0 @@
-require("aaronb.set")
-require("aaronb.packer")
